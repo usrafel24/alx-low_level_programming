198b3cc4d890c79f21f27028789d96b9a334b7c1
usrafel24 <surafeld671@gmail.com>
2023-02-17 10:44:10 -0800

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#include <studio.h>
+
+
+int main(){
+
+
+int Number=100;
+
+if(Number > 0){
+	printf ("is positive")
+		else if (Number === 0){
+			printf ("is zero") 
+		}
+		else if (Number < 0){
+			printf("is negative")
+		}else{
+			printf("err")
+		}
+
+}
+return 0
+
+
+}
+
+
+
+
+
